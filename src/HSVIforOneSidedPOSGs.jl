1eacc9036d984d355c460313c97b9d242a3e9f1b
Jakub Brož <brozjak2@fel.cvut.cz>
2021-02-12 17:46:51 +0100

    Add game tests

--- src/HSVIforOneSidedPOSGs.jl
+++ src/HSVIforOneSidedPOSGs.jl
@@ -48,12 +48,13 @@ function solve(
     epsilon::Float64,
     D::Float64,
     start::Float64,
+    verbose::Bool
 )
     while excess(initPartition, initBelief, epsilon) > 0
         globalAlphaCount = sum(length(p.gamma) for p in game.partitions)
         globalUpsilonCount = sum(length(p.upsilon) for p in game.partitions)
 
-        println(@sprintf(
+        println_if_verbose(verbose, @sprintf(
             "%7.3fs\t%+9.3f\t%+9.3f\t%+9.3f\t%5d\t%5d",
             time() - start,
             LBValue(initPartition, initBelief),
@@ -94,12 +95,13 @@ function main(
     D::Float64,
     presolveDelta::Float64,
     presolveLimit::Float64,
+    verbose::Bool=false
 )
     start = time()
     game, initPartition, initBelief = load(gameFilePath)
-    println(@sprintf("%7.3fs\tGame loaded", time() - start))
+    println_if_verbose(verbose, @sprintf("%7.3fs\tGame loaded", time() - start))
     prepare(game)
-    println(@sprintf("%7.3fs\tGame prepared", time() - start))
+    println_if_verbose(verbose, @sprintf("%7.3fs\tGame prepared", time() - start))
 
     @assert 0 < D < (1 - game.disc) * epsilon / (2 * lipschitzdelta(game)) @sprintf(
         "neighborhood parameter D = %.5f is outside bounds (%.5f, %.5f)",
@@ -108,44 +110,52 @@ function main(
         (1 - game.disc) * epsilon / (2 * lipschitzdelta(game))
     )
 
-    println("GAME:")
-    println("nStates: $(game.nStates)")
-    println("nPartitions: $(game.nPartitions)")
-    println("nLeaderActions: $(game.nLeaderActions)")
-    println("nFollowerActions: $(game.nFollowerActions)")
-    println("nObservations: $(game.nObservations)")
-    println("nTransitions: $(game.nTransitions)")
-    println("nRewards: $(game.nRewards)")
-    println("minReward: $(game.minReward)")
-    println("maxReward: $(game.maxReward)")
-    println("Lmin: $(Lmin(game))")
-    println("Umax: $(Umax(game))")
-    println("lipschitzdelta: $(lipschitzdelta(game))")
-    println("D: $D")
-    println("epsilon: $epsilon")
-    println("initPartition: $(initPartition.index)")
-    println("initBelief: $initBelief")
-    println("--------------------")
+    println_if_verbose(verbose, "GAME:")
+    println_if_verbose(verbose, "nStates: $(game.nStates)")
+    println_if_verbose(verbose, "nPartitions: $(game.nPartitions)")
+    println_if_verbose(verbose, "nLeaderActions: $(game.nLeaderActions)")
+    println_if_verbose(verbose, "nFollowerActions: $(game.nFollowerActions)")
+    println_if_verbose(verbose, "nObservations: $(game.nObservations)")
+    println_if_verbose(verbose, "nTransitions: $(game.nTransitions)")
+    println_if_verbose(verbose, "nRewards: $(game.nRewards)")
+    println_if_verbose(verbose, "minReward: $(game.minReward)")
+    println_if_verbose(verbose, "maxReward: $(game.maxReward)")
+    println_if_verbose(verbose, "Lmin: $(Lmin(game))")
+    println_if_verbose(verbose, "Umax: $(Umax(game))")
+    println_if_verbose(verbose, "lipschitzdelta: $(lipschitzdelta(game))")
+    println_if_verbose(verbose, "D: $D")
+    println_if_verbose(verbose, "epsilon: $epsilon")
+    println_if_verbose(verbose, "initPartition: $(initPartition.index)")
+    println_if_verbose(verbose, "initBelief: $initBelief")
+    println_if_verbose(verbose, "--------------------")
 
     presolveUB(game, presolveDelta, presolveLimit)
-    println(@sprintf(
+    println_if_verbose(verbose, @sprintf(
         "%7.3fs\tpresolveUB\t%+9.3f",
         time() - start,
         UBValue(initPartition, initBelief),
     ))
     presolveLB(game, presolveDelta, presolveLimit)
-    println(@sprintf(
+    println_if_verbose(verbose, @sprintf(
         "%7.3fs\tpresolveLB\t%+9.3f",
         time() - start,
         LBValue(initPartition, initBelief),
     ))
 
-    solve(game, initPartition, initBelief, epsilon, D, start)
-    println(@sprintf(
+    solve(game, initPartition, initBelief, epsilon, D, start, verbose)
+    println_if_verbose(verbose, @sprintf(
         "%7.3fs\tGame solved\t%+9.3f",
         time() - start,
         width(initPartition, initBelief),
     ))
+
+    return game, initPartition, initBelief
+end
+
+function println_if_verbose(verbose::Bool, text::String)
+    if verbose
+        println(text)
+    end
 end
 
 end

4041cfd4f98e62ed5000e9e05eebefef5da21cf8
Jakub Brož <brozjak2@fel.cvut.cz>
2021-02-13 10:10:38 +0100

    Reorder files

--- src/HSVIforOneSidedPOSGs.jl
+++ src/HSVIforOneSidedPOSGs.jl
@@ -19,6 +19,69 @@ function __init__()
     GRB_ENV[] = Gurobi.Env()
 end
 
+function main(
+    gameFilePath::String,
+    epsilon::Float64,
+    D::Float64,
+    presolveDelta::Float64,
+    presolveLimit::Float64,
+    verbose::Bool=false
+)
+    start = time()
+    game, initPartition, initBelief = load(gameFilePath)
+    println_if_verbose(verbose, @sprintf("%7.3fs\tGame loaded", time() - start))
+    prepare(game)
+    println_if_verbose(verbose, @sprintf("%7.3fs\tGame prepared", time() - start))
+
+    @assert 0 < D < (1 - game.disc) * epsilon / (2 * lipschitzdelta(game)) @sprintf(
+        "neighborhood parameter D = %.5f is outside bounds (%.5f, %.5f)",
+        D,
+        0,
+        (1 - game.disc) * epsilon / (2 * lipschitzdelta(game))
+    )
+
+    println_if_verbose(verbose, "GAME:")
+    println_if_verbose(verbose, "nStates: $(game.nStates)")
+    println_if_verbose(verbose, "nPartitions: $(game.nPartitions)")
+    println_if_verbose(verbose, "nLeaderActions: $(game.nLeaderActions)")
+    println_if_verbose(verbose, "nFollowerActions: $(game.nFollowerActions)")
+    println_if_verbose(verbose, "nObservations: $(game.nObservations)")
+    println_if_verbose(verbose, "nTransitions: $(game.nTransitions)")
+    println_if_verbose(verbose, "nRewards: $(game.nRewards)")
+    println_if_verbose(verbose, "minReward: $(game.minReward)")
+    println_if_verbose(verbose, "maxReward: $(game.maxReward)")
+    println_if_verbose(verbose, "Lmin: $(Lmin(game))")
+    println_if_verbose(verbose, "Umax: $(Umax(game))")
+    println_if_verbose(verbose, "lipschitzdelta: $(lipschitzdelta(game))")
+    println_if_verbose(verbose, "D: $D")
+    println_if_verbose(verbose, "epsilon: $epsilon")
+    println_if_verbose(verbose, "initPartition: $(initPartition.index)")
+    println_if_verbose(verbose, "initBelief: $initBelief")
+    println_if_verbose(verbose, "--------------------")
+
+    presolveUB(game, presolveDelta, presolveLimit)
+    println_if_verbose(verbose, @sprintf(
+        "%7.3fs\tpresolveUB\t%+9.3f",
+        time() - start,
+        UBValue(initPartition, initBelief),
+    ))
+    presolveLB(game, presolveDelta, presolveLimit)
+    println_if_verbose(verbose, @sprintf(
+        "%7.3fs\tpresolveLB\t%+9.3f",
+        time() - start,
+        LBValue(initPartition, initBelief),
+    ))
+
+    solve(game, initPartition, initBelief, epsilon, D, start, verbose)
+    println_if_verbose(verbose, @sprintf(
+        "%7.3fs\tGame solved\t%+9.3f",
+        time() - start,
+        width(initPartition, initBelief),
+    ))
+
+    return game, initPartition, initBelief
+end
+
 function presolveUB(game::Game, presolveDelta::Float64, presolveLimit::Float64)
     U = Umax(game)
 
@@ -87,69 +150,6 @@ function explore(partition, belief, rho, D)
         _, _, y = computeUBdual(partition, belief)
         pointBasedUpdate(partition, belief, alpha, y)
     end
-end
-
-function main(
-    gameFilePath::String,
-    epsilon::Float64,
-    D::Float64,
-    presolveDelta::Float64,
-    presolveLimit::Float64,
-    verbose::Bool=false
-)
-    start = time()
-    game, initPartition, initBelief = load(gameFilePath)
-    println_if_verbose(verbose, @sprintf("%7.3fs\tGame loaded", time() - start))
-    prepare(game)
-    println_if_verbose(verbose, @sprintf("%7.3fs\tGame prepared", time() - start))
-
-    @assert 0 < D < (1 - game.disc) * epsilon / (2 * lipschitzdelta(game)) @sprintf(
-        "neighborhood parameter D = %.5f is outside bounds (%.5f, %.5f)",
-        D,
-        0,
-        (1 - game.disc) * epsilon / (2 * lipschitzdelta(game))
-    )
-
-    println_if_verbose(verbose, "GAME:")
-    println_if_verbose(verbose, "nStates: $(game.nStates)")
-    println_if_verbose(verbose, "nPartitions: $(game.nPartitions)")
-    println_if_verbose(verbose, "nLeaderActions: $(game.nLeaderActions)")
-    println_if_verbose(verbose, "nFollowerActions: $(game.nFollowerActions)")
-    println_if_verbose(verbose, "nObservations: $(game.nObservations)")
-    println_if_verbose(verbose, "nTransitions: $(game.nTransitions)")
-    println_if_verbose(verbose, "nRewards: $(game.nRewards)")
-    println_if_verbose(verbose, "minReward: $(game.minReward)")
-    println_if_verbose(verbose, "maxReward: $(game.maxReward)")
-    println_if_verbose(verbose, "Lmin: $(Lmin(game))")
-    println_if_verbose(verbose, "Umax: $(Umax(game))")
-    println_if_verbose(verbose, "lipschitzdelta: $(lipschitzdelta(game))")
-    println_if_verbose(verbose, "D: $D")
-    println_if_verbose(verbose, "epsilon: $epsilon")
-    println_if_verbose(verbose, "initPartition: $(initPartition.index)")
-    println_if_verbose(verbose, "initBelief: $initBelief")
-    println_if_verbose(verbose, "--------------------")
-
-    presolveUB(game, presolveDelta, presolveLimit)
-    println_if_verbose(verbose, @sprintf(
-        "%7.3fs\tpresolveUB\t%+9.3f",
-        time() - start,
-        UBValue(initPartition, initBelief),
-    ))
-    presolveLB(game, presolveDelta, presolveLimit)
-    println_if_verbose(verbose, @sprintf(
-        "%7.3fs\tpresolveLB\t%+9.3f",
-        time() - start,
-        LBValue(initPartition, initBelief),
-    ))
-
-    solve(game, initPartition, initBelief, epsilon, D, start, verbose)
-    println_if_verbose(verbose, @sprintf(
-        "%7.3fs\tGame solved\t%+9.3f",
-        time() - start,
-        width(initPartition, initBelief),
-    ))
-
-    return game, initPartition, initBelief
 end
 
 function println_if_verbose(verbose::Bool, text::String)
--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,17 @@
-function aoPairProbability(partition::Partition, belief::Array{Float64,1},
-    policy1::Dict{Int64,Float64}, policy2::Dict{Tuple{Int64,Int64},Float64}, a1::Int64, o::Int64)
-    return sum(belief[partition.game.states[t[1]].inPartitionIndex] * policy1[a1] * policy2[(t[1], t[3])] * t[6]
-               for t in partition.aoTransitions[(a1, o)])
+function selectAOPair(partition::Partition, belief::Array{Float64,1}, policy1::Dict{Int64,Float64}, policy2::Dict{Tuple{Int64,Int64},Float64}, rho::Float64)
+    weightedExcessGaps = Dict((a1, o) => weightedExcess(partition, belief, policy1, policy2, a1, o, rho) for a1 in partition.leaderActions for o in partition.observations[a1])
+
+    _, (a1, o) = findmax(weightedExcessGaps)
+
+    return a1, o
+end
+
+function weightedExcess(partition::Partition, belief::Array{Float64,1}, policy1::Dict{Int64,Float64}, policy2::Dict{Tuple{Int64,Int64},Float64}, a1::Int64, o::Int64, rho::Float64)
+    transformedBelief = beliefTransform(partition, belief, policy1, policy2, a1, o)
+    nextPartition = partition.game.partitions[partition.partitionTransitions[(a1, o)]]
+
+    return (aoPairProbability(partition, belief, policy1, policy2, a1, o)
+           * excess(nextPartition, transformedBelief, rho))
 end
 
 function beliefTransform(partition::Partition, belief::Array{Float64,1},
@@ -18,26 +28,16 @@ function beliefTransform(partition::Partition, belief::Array{Float64,1},
     return aoProbInv * targetBelief
 end
 
-function nextRho(prevRho::Float64, game::Game, D::Float64)
-    return (prevRho - 2 * lipschitzdelta(game) * D) / game.disc
+function aoPairProbability(partition::Partition, belief::Array{Float64,1},
+    policy1::Dict{Int64,Float64}, policy2::Dict{Tuple{Int64,Int64},Float64}, a1::Int64, o::Int64)
+    return sum(belief[partition.game.states[t[1]].inPartitionIndex] * policy1[a1] * policy2[(t[1], t[3])] * t[6]
+               for t in partition.aoTransitions[(a1, o)])
 end
 
 function excess(partition::Partition, belief::Array{Float64,1}, rho::Float64)
     return width(partition, belief) - rho
 end
 
-function weightedExcess(partition::Partition, belief::Array{Float64,1}, policy1::Dict{Int64,Float64}, policy2::Dict{Tuple{Int64,Int64},Float64}, a1::Int64, o::Int64, rho::Float64)
-    transformedBelief = beliefTransform(partition, belief, policy1, policy2, a1, o)
-    nextPartition = partition.game.partitions[partition.partitionTransitions[(a1, o)]]
-
-    return (aoPairProbability(partition, belief, policy1, policy2, a1, o)
-           * excess(nextPartition, transformedBelief, rho))
-end
-
-function selectAOPair(partition::Partition, belief::Array{Float64,1}, policy1::Dict{Int64,Float64}, policy2::Dict{Tuple{Int64,Int64},Float64}, rho::Float64)
-    weightedExcessGaps = Dict((a1, o) => weightedExcess(partition, belief, policy1, policy2, a1, o, rho) for a1 in partition.leaderActions for o in partition.observations[a1])
-
-    _, (a1, o) = findmax(weightedExcessGaps)
-
-    return a1, o
+function nextRho(prevRho::Float64, game::Game, D::Float64)
+    return (prevRho - 2 * lipschitzdelta(game) * D) / game.disc
 end

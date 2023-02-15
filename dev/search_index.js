43e91e6e5c4d470ea4da12e07b72bdc0993e978f
Documenter.jl <documenter@juliadocs.github.io>
2023-02-15 17:10:36 +0000

    build based on 4a1d706

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HSVIforOSPOSGs","category":"page"},{"location":"#HSVIforOSPOSGs","page":"Home","title":"HSVIforOSPOSGs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HSVIforOSPOSGs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HSVIforOSPOSGs]","category":"page"}]
+}

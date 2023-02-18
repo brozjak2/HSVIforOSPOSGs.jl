baa98c247f1b40816d287ce3d673de42d6a3e444
Jakub Brož <brozjak2@fel.cvut.cz>
2023-02-18 07:39:30 +0100

    Add solve tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,17 @@ using Test
 using Logging
 
 @testset "HSVIforOSPOSGs.jl" begin
-    # Write your tests here.
+    @testset "solve" begin
+        with_logger(ConsoleLogger(stdout, Logging.Error)) do
+            osposg = OSPOSG("../games/pursuit-evasion/peg03.osposg")
+            hsvi = HSVI()
+            epsilon = 1.0
+            time_limit = 60.0
+
+            recorder = solve(osposg, hsvi, epsilon, time_limit)
+
+            @test HSVIforOSPOSGs.width(osposg, hsvi) < epsilon
+            @test HSVIforOSPOSGs.LB_value(osposg, hsvi) < 83.443625 < HSVIforOSPOSGs.UB_value(osposg, hsvi)
+        end
+    end
 end
